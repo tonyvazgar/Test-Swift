c460c2add11ca2149e32bef33c02e29a0b132c2b
tonyvazgar <luis.vazquezga@udlap.mx>
2018-08-17 18:02:17 -0500

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Tony Vazgar on 8/17/18.
+//  Copyright © 2018 Tony Vazgar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
